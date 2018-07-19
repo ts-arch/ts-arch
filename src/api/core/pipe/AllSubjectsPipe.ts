0e37ecfa47f60cc82a21adc97435d83e12097af5
mrpatpat <adrian.endrich@googlemail.com>
2018-07-19 22:21:14 +0200

    feat: add pre and suffix name matcher

--- src/api/core/pipe/AllSubjectsPipe.ts
+++ src/api/core/pipe/AllSubjectsPipe.ts
@@ -14,6 +14,14 @@ export class AllSubjectsPipe extends ArchRulePipe {
     return new SubjectsNameFilterPipe(this, regex)
   }
 
+  public withNamePrefix(prefix: string): SubjectsNameFilterPipe {
+    return new SubjectsNameFilterPipe(this, new RegExp(prefix + '.+.($|\\n)'))
+  }
+
+  public withNameSuffix(suffix: string): SubjectsNameFilterPipe {
+    return new SubjectsNameFilterPipe(this, new RegExp('.+.' + suffix + '($|\\n)'))
+  }
+
   public should(): ShouldPipe {
     return new ShouldPipe(this)
   }
--- src/api/rules.spec.ts
+++ src/api/rules.spec.ts
@@ -47,4 +47,46 @@ describe('Rules', () => {
       .haveSubjects()
     expect(rule.check(subjects)).toBeTruthy()
   })
+
+  it('should find all controllers', () => {
+    const pipe = TSArch.defineThat()
+      .allSubjects()
+      .withNameMatching(/Controller/)
+    expect(pipe.filterSubjects(subjects)).toMatchSnapshot()
+  })
+
+  it('should find all Dog stuff', () => {
+    const pipe = TSArch.defineThat()
+      .allSubjects()
+      .withNameMatching(/Dog/)
+    expect(pipe.filterSubjects(subjects)).toMatchSnapshot()
+  })
+
+  it('should find all Services by suffix', () => {
+    const pipe = TSArch.defineThat()
+      .allSubjects()
+      .withNameSuffix('Service') //TODO this is the wrong result
+    expect(pipe.filterSubjects(subjects)).toMatchSnapshot()
+  })
+
+  it('should find all Dog things by prefix', () => {
+    const pipe = TSArch.defineThat()
+      .allSubjects()
+      .withNamePrefix('Dog') //TODO this is the wrong result
+    expect(pipe.filterSubjects(subjects)).toMatchSnapshot()
+  })
+
+  it('should not find all Dog things by suffix', () => {
+    const pipe = TSArch.defineThat()
+      .allSubjects()
+      .withNameSuffix('Dog') //TODO this is the wrong result
+    expect(pipe.filterSubjects(subjects)).toMatchSnapshot()
+  })
+
+  it('should not find all Services by prefix', () => {
+    const pipe = TSArch.defineThat()
+      .allSubjects()
+      .withNamePrefix('Service') //TODO this is the wrong result
+    expect(pipe.filterSubjects(subjects)).toMatchSnapshot()
+  })
 })
