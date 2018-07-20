6b645c81c884b1f165af9ec398b734a68778105e
Adrian Endrich <adrian.endrich@maibornwolff.de>
2018-07-20 15:35:37 +0200

    refactor(jest matcher): move jest matcher to separate file

---
+++ src/jest-ts-arch/TsArchMatchers.ts
@@ -0,0 +1,31 @@
+import { ArchProject } from '../api/core/ArchProject'
+import { ArchRule } from '../api/core/abstract/ArchRule'
+
+expect.extend({
+  toMatchArchRule(project: ArchProject, ruleToMatch: ArchRule) {
+    if (project === null) {
+      return {
+        pass: false,
+        message: () => 'expected project as input'
+      }
+    }
+
+    if (ruleToMatch === null) {
+      return {
+        pass: false,
+        message: () => 'expected rule to match against'
+      }
+    }
+
+    const result = ruleToMatch.checkProject(project)
+    if (this.isNot) {
+      return {
+        pass: !result
+      }
+    } else {
+      return {
+        pass: result
+      }
+    }
+  }
+})
--- test/parser.spec.ts
+++ test/parser.spec.ts
@@ -1,37 +1,5 @@
 import { TSArch } from '../src/tsarch'
-import { ArchProject } from '../src/api/core/ArchProject'
-import { ArchRule } from '../src/api/core/abstract/ArchRule'
-
-//TODO make it importable as a matcher
-//TODO error messages ?
-expect.extend({
-  toMatchArchRule(project: ArchProject, ruleToMatch: ArchRule) {
-    if (project === null) {
-      return {
-        pass: false,
-        message: () => 'expected project as input'
-      }
-    }
-
-    if (ruleToMatch === null) {
-      return {
-        pass: false,
-        message: () => 'expected rule to match against'
-      }
-    }
-
-    const result = ruleToMatch.checkProject(project)
-    if (this.isNot) {
-      return {
-        pass: !result
-      }
-    } else {
-      return {
-        pass: result
-      }
-    }
-  }
-})
+import '../src/jest-ts-arch/TsArchMatchers'
 
 describe('Parser', () => {
   it('all files in pipes folder of this project should be named ...Pipe.ts', async () => {
