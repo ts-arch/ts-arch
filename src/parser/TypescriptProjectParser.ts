380e5cc0dcfa1bad60ac699a119324384eba230b
Adrian Endrich <adrian.endrich@maibornwolff.de>
2018-07-20 15:29:50 +0200

    refactor(parser): move code to correct location

--- src/parser/TypescriptProjectParser.ts
+++ src/parser/TypescriptProjectParser.ts
@@ -1,17 +1,24 @@
 import { File, TypescriptParser } from 'typescript-parser'
 import { Glob } from 'glob'
+import { ArchProject } from '../api/core/ArchProject'
+import { FileSubject } from '../api/core/subject/FileSubject'
 
 export class TypescriptProjectParser {
-  public async parse(rootPath: string): Promise<File[]> {
+  public async parse(rootPath: string): Promise<ArchProject> {
     const parser = new TypescriptParser()
     const fileNames = await this.getFileNames(rootPath + '/**/*.ts')
     const parsed: File[] = await parser.parseFiles(fileNames, 'ROOT')
-    return parsed
+    const project = new ArchProject()
+    parsed.forEach(file => {
+      const fileSubject = new FileSubject(file.parsedPath.base, file.parsedPath.dir)
+      project.addSubject(fileSubject)
+    })
+    return project
   }
 
   public async getFileNames(globPattern: string): Promise<string[]> {
     return new Promise((resolve, reject) => {
-      Glob(globPattern, (err, files) => {
+      Glob(globPattern, (err, files: string[]) => {
         if (err) {
           reject(err)
           return
--- src/tsarch.ts
+++ src/tsarch.ts
@@ -1,6 +1,5 @@
 import { EntryPipe } from './api/core/pipe/EntryPipe'
 import { TypescriptProjectParser } from './parser/TypescriptProjectParser'
-import { FileSubject } from './api/core/subject/FileSubject'
 import { ArchProject } from './api/core/ArchProject'
 
 export class TSArch {
@@ -10,12 +9,7 @@ export class TSArch {
 
   static async parseTypescriptProject(path: string): Promise<ArchProject> {
     const parser = new TypescriptProjectParser()
-    const files = await parser.parse(path)
-    const project = new ArchProject()
-    files.forEach(file => {
-      const fileSubject = new FileSubject(file.parsedPath.base, file.parsedPath.dir)
-      project.addSubject(fileSubject)
-    })
+    const project = await parser.parse(path)
     return project
   }
 }
