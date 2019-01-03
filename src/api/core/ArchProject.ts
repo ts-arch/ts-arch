e0d38434164e3ddaa1f5a32c02992d96e0ccbc42
mrpatpat <adrian.endrich@googlemail.com>
2019-01-03 16:32:03 +0100

    style: use tabs instead of spaces for indentation#

--- src/api/core/ArchProject.ts
+++ src/api/core/ArchProject.ts
@@ -1,21 +1,13 @@
-import { ArchSubject } from './abstract/ArchSubject'
+import { ArchSubject } from "./abstract/ArchSubject"
 
 export class ArchProject {
-  constructor(private subjects: ArchSubject[] = []) {}
+	constructor(private subjects: ArchSubject[] = []) {}
 
-  public addSubject(subject: ArchSubject) {
-    this.subjects.push(subject)
-  }
+	public addSubject(subject: ArchSubject) {
+		this.subjects.push(subject)
+	}
 
-  public getSubjects(): ArchSubject[] {
-    return this.subjects
-  }
-
-  public sayHello() {
-    console.log('hello')
-  }
-
-  public sayBye() {
-    console.log('bye')
-  }
+	public getSubjects(): ArchSubject[] {
+		return this.subjects
+	}
 }
