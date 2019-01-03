92db1bd8245582dbea539b0196441a6f17d8dfdb
mrpatpat <adrian.endrich@googlemail.com>
2019-01-03 23:55:14 +0100

    refactor: adds typings to jest matcher

--- src/jest/ArchMatchers.ts
+++ src/jest/ArchMatchers.ts
@@ -1,31 +1,43 @@
-import { ArchProject } from '../api/core/ArchProject'
-import { ArchRule } from '../api/core/abstract/ArchRule'
+import { ArchProject } from "../api/core/ArchProject"
+import { ArchRule } from "../api/core/abstract/ArchRule"
+
+function buildJestResult(pass: boolean, msg: string): { pass: boolean; message: () => string } {
+	return { pass: pass, message: () => msg }
+}
+
+export function toMatchArchRuleLogic(
+	jestCtx: { isNot: boolean },
+	project?: ArchProject,
+	ruleToMatch?: ArchRule
+): { pass: boolean; message?: () => string } {
+	if (!project) {
+		return buildJestResult(false, "expected project as input")
+	} else if (!ruleToMatch) {
+		return buildJestResult(false, "expected rule to match against")
+	} else {
+		const result = ruleToMatch.checkProject(project)
+		if (jestCtx.isNot) {
+			return buildJestResult(!result, "expected to not pass rule")
+		} else {
+			return buildJestResult(result, "expected to pass rule")
+		}
+	}
+}
+
+/*
+ * Extending Jest and its type
+ */
+declare global {
+	namespace jest {
+		// tslint:disable-next-line:interface-name
+		interface Matchers<R> {
+			toMatchArchRule(ruleToMatch: ArchRule): R
+		}
+	}
+}
 
 expect.extend({
-  toMatchArchRule(project: ArchProject | null, ruleToMatch: ArchRule | null) {
-    if (project === null) {
-      return {
-        pass: false,
-        message: () => 'expected project as input'
-      }
-    }
-
-    if (ruleToMatch === null) {
-      return {
-        pass: false,
-        message: () => 'expected rule to match against'
-      }
-    }
-
-    const result = ruleToMatch.checkProject(project)
-    if (this.isNot) {
-      return {
-        pass: !result
-      }
-    } else {
-      return {
-        pass: result
-      }
-    }
-  }
+	toMatchArchRule(project?: ArchProject, ruleToMatch?: ArchRule) {
+		return toMatchArchRuleLogic(this, project, ruleToMatch)
+	}
 } as any)
