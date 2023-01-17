84b6e7f92de2cd566b63fb167ed96ccc1c118fce
torfmaster <briefe@kebes.de>
2023-01-17 09:58:13 +0100

    chore: format all project files to make ci green

--- src/common/assertion/violation.ts
+++ src/common/assertion/violation.ts
@@ -1,1 +1,1 @@
-export interface Violation{}
+export interface Violation {}
--- src/common/util/regexUtils.ts
+++ src/common/util/regexUtils.ts
@@ -1,6 +1,4 @@
-export function matchingAllPatterns(input: string, patterns: Array<string|RegExp>): boolean {
-	const matches = patterns
-		.map(p => input.match(p))
-		.map(m => m != null && m.length > 0)
+export function matchingAllPatterns(input: string, patterns: Array<string | RegExp>): boolean {
+	const matches = patterns.map((p) => input.match(p)).map((m) => m != null && m.length > 0)
 	return matches.indexOf(false) === -1
 }
