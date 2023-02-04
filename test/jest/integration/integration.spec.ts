7456cc65548f6ee05fc3b3e15a80d186aa40011a
torfmaster <briefe@kebes.de>
2023-02-04 16:29:50 +0100

    fix: fix escaping in integration test
    Otherwise the `.` would be interpreted as an operator in the expression than just a dot.
    
    Co-authored-by: Charlie Harding <charlie_harding@icloud.com>

--- test/jest/integration/integration.spec.ts
+++ test/jest/integration/integration.spec.ts
@@ -6,7 +6,7 @@ describe("Integration test", () => {
 		const rule = (await filesOfProject(__dirname + "/samples/filenamingsample/tsconfig.json"))
 			.inFolder("services")
 			.should()
-			.matchPattern(".*Service.ts")
+			.matchPattern(".*Service\\.ts")
 
 		await expect(rule).not.toPassAsync()
 	})
